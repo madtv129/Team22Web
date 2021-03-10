597ce5fde74919890a2c0f93a63f7b20a9049406
madtv129 <madtv129@gmail.com>
2021-03-10 11:12:08 -0800

    login stuff and api key

---
+++ scripts/firebase_api_key.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBLwANiwjF4qU2bXeb1mc6jeDkn-HVm-KA",
+  authDomain: "lastmeal-8526f.firebaseapp.com",
+  databaseURL: "https://lastmeal-8526f-default-rtdb.firebaseio.com",
+  projectId: "lastmeal-8526f",
+  storageBucket: "lastmeal-8526f.appspot.com",
+  messagingSenderId: "155424529573",
+  appId: "1:155424529573:web:2d68211dc51148c544014c",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
